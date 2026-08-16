e624311028ecaa719f3a798bd2da989ec1e69f59
M Ridwan <mridwan07072002@gmail.com>
2026-08-16 15:24:41 +0700

    feat: add dynamic DB_SSLMODE for cloud postgres support

--- app/database/db.go
+++ app/database/db.go
@@ -12,13 +12,19 @@ import (
 
 // Connect membuka koneksi ke PostgreSQL menggunakan GORM dan menjalankan AutoMigrate.
 func Connect() *gorm.DB {
+	sslmode := os.Getenv("DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Jakarta",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
+		sslmode,
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
